c43b72568135af8d114bcbf37252d87d23854b9d
Sarabjeet Singh <sjalalpuria001@gmail.com>
2025-08-04 18:13:01 +0530

    updates

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Sarabjeet Singh",
-  profile: '/profile.jpg',
+  profile: '/profile1.jpg',
   designation: "Software Developer",
   description: "Hi, I'm Sarabjeet Singh — a passionate and enthusiastic programmer with a strong focus on web application development. I’m a quick and self-motivated learner who loves diving into new technologies and solving real-world problems through code. My development journey is deeply rooted in JavaScript, but I also work extensively with frameworks and tools like Laravel, Next.js, Node.js, React, Yii, and more.I enjoy building scalable, high- performance web applications and aim to make the web more accessible and innovative. I'm always open to exciting opportunities that align with my skills and interests — whether it’s freelance, collaboration, or full-time roles.",
   email: 'sjalalpuria001@gmail.com',
