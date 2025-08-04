1ed0e19729cda0d797db6e78acd4dc8720446c29
Sarabjeet Singh <sjalalpuria001@gmail.com>
2025-08-04 18:05:55 +0530

    changes

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -9,19 +9,18 @@ export const educations = [
     id: 2,
     title: "Bachelor Degree (BCA)",
     duration: "2017-2020",
-    institution: "Post Graduate Govt College, Sector 46B, Chandigarh"
+    institution: "Panjab University, Chandigarh",
   },
   {
     id: 3,
-    title: " +2 ",
+    title: " Senior Secondary (12th)",
     duration: "2016 - 2017",
-    institution: "Govt Sen Sec School , Boota Singh Wala",
+    institution: "Punjab School Education Board, Mohali, Punjab",
   },
   {
     id: 4,
-    title: "10th",
+    title: "Matriculation (10th)",
     duration: "2015 - 2016",
-    institution: "Govt Sen Sec School , Jansla",
-
+    institution: "Punjab School Education Board, Mohali, Punjab",
   }
 ]
